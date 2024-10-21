6c30e6b25e6aa957aa40d341d54e2c5302edb101
fvalto <skualo_fv@msn.com>
2024-10-20 22:18:14 -0300

    index, logo, some css, header and footer using js

---
+++ js/utils.mjs
@@ -0,0 +1,19 @@
+export function qs(selector, parent=document) {
+    return parent.querySelector(selector);
+}
+
+export async function renderTemplate(partialFileName, parentElement, position='afterbegin'){
+    const template = await getTemplate(partialFileName);
+    parentElement.innerHTML='';
+    parentElement.insertAdjacentHTML(position, template);
+}
+
+export async  function loadHeaderFooter(){
+    renderTemplate('header.html',qs('#header'));
+    renderTemplate('footer.html',qs('#footer'));
+}
+
+async function getTemplate(fileName) {
+    const path = `/templates/${fileName}`;
+    return await fetch(path).then((data) => data.text());
+}
